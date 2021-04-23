e85f438f36ea3b1c1e81a8cce820e33bcfca71c1
Benjamin Isong <beisongabc@gmail.com>
2021-04-23 12:43:41 +0100

    small fix

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         factory(User::class, 1)->create([
-            'email' => 'admin@app.com',
+            'email' => 'benjamin@finesseintegrated.com',
         ]);
     }
 }
