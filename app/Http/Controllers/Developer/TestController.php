e34ee119ac84d63fe8b35ca0f49f8626a364c049
Benjamin Isong <beisongabc@gmail.com>
2021-10-17 12:04:17 +0100

    small fix

--- app/Http/Controllers/Developer/TestController.php
+++ app/Http/Controllers/Developer/TestController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Developer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Record;
 use App\Traits\Email\Mailer;
 use Illuminate\Http\Request;
+use Illuminate\Database\Query\Builder;
 
 class TestController extends Controller
 {
@@ -15,5 +17,27 @@ class TestController extends Controller
         ];
         $this->sendMail('', 'Test Email',$email,'Testing the email','Ben Ice',$data,'emails.test');
         return response()->json(['completed']);
+    }
+
+    public function pending($user_id){
+        $pending =
+            Record::query()
+                ->whereIn('records.uuid',function(Builder $query) use ($user_id) {
+                    $query->from('office_slips')
+                        ->select('office_slips.record_id')
+                        ->where('office_slips.status','=','pending')
+                        ->where('office_slips.current','=',true)
+                        ->whereIn('office_slips.office_id', function (Builder $q) use ($user_id){
+                            $q->from('office_members')
+                                ->select('office_members.office_id')
+                                ->where('office_members.user_id', $user_id);
+                        })
+                    ;
+                })
+                ->where('completed', '!=',true)
+                ->orderBy('id','desc')
+                ->get();
+
+        return $pending;
     }
 }
--- resources/views/pages/dashboard/pendings.blade.php
+++ resources/views/pages/dashboard/pendings.blade.php
@@ -3,22 +3,25 @@
         <h5 class="nk-block-title fw-normal mb-3">You have {{ $pending->count() }} Pending Records that needs your attention</h5>
         <div class="row g-gs">
             @foreach($pending as $record)
-                <div class="col-md-4">
-                    <div class="card card-bordered card-full">
-                        <div class="nk-wg1">
-                            <div class="nk-wg1-block">
-                                <div class="nk-wg1-text">
-                                    <h5 class="title">{{ $record->CurrentOfficeSlip->office->name }}'s Office</h5>
-                                    <p>{{ $record->title }}.</p>
-                                    <small>Submitted by <b>{{ $record->CurrentOfficeSlip->user->names }}</b> on {{ date('F d, Y', strtotime($record->CurrentOfficeSlip->created_at)) }} </small>
+                @if(!empty($record->CurrentOfficeSlip))
+                    <div class="col-md-4">
+                        <div class="card card-bordered card-full">
+                            <div class="nk-wg1">
+                                <div class="nk-wg1-block">
+                                    <div class="nk-wg1-text">
+                                        <h5 class="title">{{ $record->CurrentOfficeSlip->office->name }}'s Office</h5>
+                                        <p>{{ $record->title }}.</p>
+                                        <small>Submitted by <b>{{ $record->CurrentOfficeSlip->user->names }}</b> on {{ date('F d, Y', strtotime($record->CurrentOfficeSlip->created_at)) }} </small>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="nk-wg1-action">
-                                <a href="{{ route('record.audit', $record->uuid) }}" class="link"><span>Review Record</span> <em class="icon ni ni-chevron-right"></em></a>
+                                <div class="nk-wg1-action">
+                                    <a href="{{ route('record.audit', $record->uuid) }}" class="link"><span>Review Record</span> <em class="icon ni ni-chevron-right"></em></a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @endif
+
             @endforeach
         </div><!-- .row -->
     </div>
